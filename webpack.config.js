317e5dcbf82c634c44093df30e3eeb1c2bea13c2
M Shashanka <56446258+Shaky-Adino@users.noreply.github.com>
2022-07-06 23:32:38 +0530

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname,'src/js/index.js'),
+    mode: 'production',
+    entry: path.resolve(__dirname,'src/index.js'),
     output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js',
         library: {
             name: 'lib',
             type: 'var',
@@ -14,21 +12,36 @@ module.exports = {
     },
     module: {
         rules: [
-          {
-            test: /\.css$/,
-            use: [
-              'style-loader',
-              'css-loader'
-            ]
+            {
+              test: /\.css$/,
+              use: [
+                'style-loader',
+                'css-loader'
+              ]
+            },
+            {
+              test: /\.(js|jsx)$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: "babel-loader"
+              }
+            },
+            {
+              test: /\.html$/,
+              use: [
+                  {
+                      loader: "html-loader"
+                  }
+              ]
           }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Graph Visualizer',
-            filename: 'index.html',
-            template: 'src/index.html',
-            favicon: "src/favicon.ico",
+            filename: './index.html',
+            template: './src/index.html',
+            favicon: "./src/favicon.ico",
         }),
     ]
 }
